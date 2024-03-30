ecfa17691b8b937eeba60f3b7726fe06c643f1dc
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 10:06:15 +0000

    Error handling for routes not found

--- app.js
+++ app.js
@@ -22,4 +22,15 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+// Add routes here
+
+// handle all the urls that couldn't be handled 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
