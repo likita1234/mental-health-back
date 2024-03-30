8cf50fe4781dd8f5338b5bdbe862c32049a1cfde
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 10:06:24 +0000

    Validity checks of questions not correct fixed in section validators

--- validators/section-validators.js
+++ validators/section-validators.js
@@ -4,15 +4,19 @@ exports.validateQuestionIds = async (questions) => {
   // Check if questions exists, then verify all the questions that exists are valid
   if (questions && questions.length > 0) {
     //  Check if each question Id in req.body.questions exists in the Question model
-    const invalidQuestionIds = await Question.find({
+    const validQuestions = await Question.find({
       _id: { $in: questions },
-    }).select('_id');
-
-    // Check if all question Ids are valid
-    const isValidQuestions = questions.every((questionId) =>
-      invalidQuestionIds.includes(questionId.toString())
+    });
+    // .select('_id');
+    // Extract only the ids and convert them into string type to make comparisons easier
+    const validQuestionsIds = validQuestions?.map((question) =>
+      question._id.toString()
     );
-    return isValidQuestions;
+    // Check if all question Ids are valid
+    const questionsValid = questions.every((question) => {
+      return validQuestionsIds.includes(question.toString());
+    });
+    return questionsValid;
   }
   return true;
 };
