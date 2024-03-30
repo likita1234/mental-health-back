10e312fd680b088d1d7a1fbdacb2d89817dd5274
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 10:06:15 +0000

    Error handlers configured

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 
 const app = express();
 
+const globalErrorHandler = require('./controller/error-controller');
+const AppError = require('./utils/app-errors');
+
 // Middleware morgan has been added as it is simply a HTTP request logger middleware for node.js
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
@@ -13,11 +16,6 @@ app.use(express.json());
 // This will serve your public folder where you can put your static files and use throughout the application
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//   console.log('Hello from the middleware 👋');
-//   next();
-// });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -25,12 +23,11 @@ app.use((req, res, next) => {
 
 // Add routes here
 
-// handle all the urls that couldn't be handled 
+// handle all the urls that couldn't be handled
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ utils/errors.js
@@ -0,0 +1,11 @@
+const errors = Object.freeze({
+  ERROR: 'Error',
+  NOT_FOUND: 'Not Found',
+  FAILED: 'Failed',
+  UNDEFINED: 'Undefined',
+  INVALID: 'Invalid',
+  UNAUTHENTICATED: 'Unauthenticated',
+  UNAUTHORIZED: 'Unauthorized',
+});
+
+module.exports = { errors };
