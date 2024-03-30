6ea9aa612b7044ecf063419dfdf64042619c3618
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 10:06:26 +0000

    LanguageSchema changes:- nepalese changed to nepali

--- models/language-schema.js
+++ models/language-schema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const LanguageSchema = new mongoose.Schema({
   // Define properties for each language
   english: { type: String, trim: true },
-  nepalese: { type: String, trim: true },
+  nepali: { type: String, trim: true },
   // Add more languages as needed
 });
 
