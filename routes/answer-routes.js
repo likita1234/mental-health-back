72fda8685e7d5885c9f92d0159e0811f661b88d6
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 10:06:31 +0000

    Route added for Question wise data analysis functionality

--- routes/answer-routes.js
+++ routes/answer-routes.js
@@ -7,6 +7,19 @@ const router = express.Router();
 
 router.route('/').get(answerController.getAllAnswers);
 
-router.get('/:category', answerController.aggregateGenderData);
+// router.get('/:category', answerController.aggregateGenderData);
+
+// =================> Question wise generic data analysis
+// _________________> Params:- formId and questionId and expected chart type:- table, pie, bar, ratings, radar
+router.get('/question', answerController.questionWiseDataAnalysis);
+
+// ______________________________________________________
+// ======================================================
+
+// =================> WHO-5 Summation Calculation data analysis
+
+// =================> Section Wise :- All Questions Included analysis (Filter out open end)
+
+// =================> All Sections Ratings Questions based :- Correlation analysis (Pearson) and Regression Analysis
 
 module.exports = router;
