6757bf9c458e75a0028041f29f89ffd95aac22cc
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 12:02:17 +0000

    Added new api that takes source and destination question ids respectively and generates answers duplications

--- routes/answer-routes.js
+++ routes/answer-routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
-// const authController = require('../controller/auth-controller');
+const authController = require('../controller/auth-controller');
 const answerController = require('../controller/answer-controller');
 
 const router = express.Router();
 
 router.route('/').get(answerController.getAllAnswers);
 
+router
+  .route('/duplicate/:sourceQuestionId/:destinationQuestionId')
+  .post(authController.validateToken, answerController.duplicateAnswerData);
 // router.get('/:category', answerController.aggregateGenderData);
-
 
 module.exports = router;
