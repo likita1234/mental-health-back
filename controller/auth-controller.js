12c1c1b59ce4031a915a375034e80d11918ac12a
Likita Maharjan <likitaof2015@gmail.com>
2024-03-30 10:06:16 +0000

    jsonwebtoken added in the user signup process for token based authentication

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,13 +1,25 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user-model');
 
 const catchAsync = require('../utils/catch-async');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  console.log(req.body);
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    surname: req.body.surname,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.password,
+  });
+
+  // Create a token
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
