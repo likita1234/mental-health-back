a9cec27a0149c8a1639bfb7f19f738db3d297ac0
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 19:13:37 +0100

    Natural Language Processing handler created for analysis of keywords from answers

---
+++ utils/nlp-handler.js
@@ -0,0 +1,101 @@
+const natural = require('natural');
+const { WordTokenizer } = natural;
+const tokenizer = new WordTokenizer();
+
+// Define stopwords (verbs, pronouns, and articles)
+const stopwords = new Set([
+  'i',
+  'me',
+  'my',
+  'myself',
+  'we',
+  'our',
+  'ours',
+  'ourselves',
+  'you',
+  'your',
+  'yours',
+  'yourself',
+  'yourselves',
+  'he',
+  'him',
+  'his',
+  'himself',
+  'she',
+  'her',
+  'hers',
+  'herself',
+  'it',
+  'its',
+  'itself',
+  'they',
+  'them',
+  'their',
+  'theirs',
+  'themselves',
+  'what',
+  'which',
+  'who',
+  'whom',
+  'this',
+  'that',
+  'these',
+  'those',
+  'am',
+  'is',
+  'are',
+  'was',
+  'were',
+  'be',
+  'been',
+  'being',
+  'have',
+  'has',
+  'had',
+  'having',
+  'do',
+  'does',
+  'did',
+  'doing',
+  'a',
+  'an',
+  'the',
+]);
+
+exports.analyzeWordsFrequencyFromAnswers = (answers) => {
+  if (answers && answers.length > 0) {
+    // Perform analysis
+    const wordFrequency = analyzeText(answers);
+
+    // Sort word frequency by frequency
+    return Object.entries(wordFrequency)
+      .sort((a, b) => b[1] - a[1])
+      .reduce((acc, [word, frequency]) => {
+        acc[word] = frequency;
+        return acc;
+      }, {});
+  }
+  return {};
+};
+
+// Function to tokenize text, remove stopwords, and perform frequency analysis
+const analyzeText = (texts) => {
+  const wordFreq = {};
+  texts.forEach((text) => {
+    const tokens = tokenizer.tokenize(text.toLowerCase());
+    // Generate bi-grams
+    const biGrams = NGrams.bigrams(tokens);
+    biGrams.forEach((biGram) => {
+      const token = biGram.join(' '); // Convert bi-gram array to string
+      if (!stopwords.has(token)) {
+        // Exclude stopwords
+        if (wordFreq[token]) {
+          wordFreq[token]++;
+        } else {
+          wordFreq[token] = 1;
+        }
+      }
+    });
+  });
+  return wordFreq;
+};
