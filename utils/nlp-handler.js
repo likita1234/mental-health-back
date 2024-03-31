4982c9b2aab2e9a0450fb415d405483c99154ed3
Likita Maharjan <likitaof2015@gmail.com>
2024-03-31 21:38:04 +0100

    Words filtering added on basis of frequencies and word length

--- utils/nlp-handler.js
+++ utils/nlp-handler.js
@@ -35,14 +35,21 @@ exports.analyzeKeywordsFrequencyInAnswers = (answers) => {
 
 // Sort the word frequency object by frequency in descending order
 const sortedWords = (words) => {
+  // Filter out words with frequency below the threshold (e.g., 10)
+  const filteredWords = Object.fromEntries(
+    Object.entries(words).filter(([_, frequency]) => frequency > 5)
+  );
+
   // Convert object to array of key-value pairs
-  const sortedEntries = Object.entries(words).sort((a, b) =>
+  const sortedEntries = Object.entries(filteredWords).sort((a, b) =>
     a[0].localeCompare(b[0])
   );
+
   // Create a new object from the sorted key-value pairs
-  const sortedWords = {};
-  for (const [key, value] of sortedEntries) {
-    sortedWords[key] = value;
-  }
-  return sortedWords;
+  const sortedWordFreq = {};
+  sortedEntries.forEach(([key, value]) => {
+    sortedWordFreq[key] = value;
+  });
+
+  return sortedWordFreq;
 };
