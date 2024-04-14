c79a0949bfdd47f9c5e3c3be512511da193b2dc1
Likita Maharjan <likitaof2015@gmail.com>
2024-04-14 12:33:39 +0100

    FilterData variable added to add condition to avoid filter outs in case of single user wise data analysis

--- utils/nlp-handler.js
+++ utils/nlp-handler.js
@@ -1,10 +1,10 @@
-const { WordTokenizer, PorterStemmer } = require('natural');
+const { WordTokenizer } = require('natural');
 const { customStopwords } = require('./custom-stopwords');
 
 const tokenizer = new WordTokenizer();
 
 // Analysis by answers
-exports.analyzeKeywordsFrequencyInAnswers = (answers) => {
+exports.analyzeKeywordsFrequencyInAnswers = (answers, filterData = true) => {
   const words = {};
 
   // Loop through each answer in the array
@@ -30,15 +30,17 @@ exports.analyzeKeywordsFrequencyInAnswers = (answers) => {
     finalWords[token] = frequency;
   }
 
-  return sortedWords(finalWords);
+  return sortedWords(finalWords, filterData);
 };
 
 // Sort the word frequency object by frequency in descending order
-const sortedWords = (words) => {
+const sortedWords = (words, filterData) => {
   // Filter out words with frequency below the threshold (e.g., 10)
-  const filteredWords = Object.fromEntries(
-    Object.entries(words).filter(([_, frequency]) => frequency > 5)
-  );
+  const filteredWords = filterData
+    ? Object.fromEntries(
+        Object.entries(words).filter(([_, frequency]) => frequency > 5)
+      )
+    : words;
 
   // Convert object to array of key-value pairs
   const sortedEntries = Object.entries(filteredWords).sort((a, b) =>
